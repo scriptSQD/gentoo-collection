c5ad2c0d986ea985a5184b84f18a93dd780ba7de
scriptSQD <scriipt.fun@gmail.com>
2022-07-06 16:02:46 +0300

    *Modify kernel build script. *Add support for USB Mass storage devices (flash drives, PHDDs and etc.) in kernel config.

--- Yoga Slim 7 Pro/kernel/build-kernel.py
+++ Yoga Slim 7 Pro/kernel/build-kernel.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 
+from enum import Enum
 import os
 from datetime import datetime
 from time import sleep
 
-print(">> Cheecking for root:")
+class Colors(Enum):
+    LOG="\033[95m"
+    GREEN="\033[92m"
+    YELLOW="\033[93m"
+    RED="\033[91m"
+    END="\033[0m"
+
+class Logger:
+    def success(message):
+        return f"{Colors.GREEN.value}{message}{Colors.END.value}"
+
+    def warning(message):
+        return f"{Colors.YELLOW.value}{message}{Colors.END.value}"
+
+    def error(message):
+        return f"{Colors.RED.value}{message}{Colors.END.value}"
+
+    def log(message):
+        return f"{Colors.LOG.value}{message}{Colors.END.value}"
+
+print(Logger.log(">> Checking for root:\n"))
 is_root = os.geteuid() == 0;
 if not is_root:
-    print(">> This script must be run as root!")
+    print(Logger.error(">> This script must be run as root!"))
     exit(1)
 
 src_dir = "/usr/src/linux"
@@ -29,60 +50,60 @@ toolchain_flags = "CC=/usr/lib/ccache/bin/clang CXX=/usr/lib/ccache/bin/clang++ 
 
 makeopts=f"{makeflags} {toolchain_flags} CFLAGS=\"{cflags}\" CXXFLAGS=\"{cxxflags}\""
 
-print("--------------------------------------------------------------------------------\n")
-print(f">> Moving into src dir: {src_dir}!")
+print(Logger.log("--------------------------------------------------------------------------------\n"))
+print(Logger.log(f">> Moving into src dir: {src_dir}!"))
 
 chdir_resp = os.chdir(src_dir)
 if not chdir_resp == None:
-    print(f"!! Failed to move into src dir: {src_dir}!")
+    print(Logger.error(f"!! Failed to move into src dir: {src_dir}!"))
     exit(1)
 
-print(">> Now running nconfig on current build tree.")
+print(Logger.log(">> Now running nconfig on current build tree."))
 sleep(3)
 
 nconfig_resp = os.system(f"make {makeopts} nconfig")
 if not nconfig_resp == 0:
-    print(f"!! `make nconfig` exited with error code: {nconfig_resp}!")
+    print(Logger.error(f"!! `make nconfig` exited with error code: {nconfig_resp}!"))
     exit(1)
 
-print(">> Now proceeding to build kernel.")
+print(Logger.log(">> Now proceeding to build kernel."))
 sleep(3)
 
 kernelbuild_resp = os.system(f"make {makeopts} | tee {log_dir}/kernelbuild-{log_timestamp}.log")
 if not kernelbuild_resp == 0:
-    print(f"!! Kernel build failed with error code: {kernelbuild_resp}! (More info")
-    print(f">> Check out kernel build log at: {log_dir}/kernelbuild-{log_timestamp}.log")
+    print(Logger.error(f"!! Kernel build failed with error code: {kernelbuild_resp}! (More info"))
+    print(Logger.warning(f">> Check out kernel build log at: {log_dir}/kernelbuild-{log_timestamp}.log"))
     exit(1)
 
-print(">> Now proceeding to install modules.")
+print(Logger.log(">> Now proceeding to install modules."))
 sleep(3)
 
 modinstall_resp = os.system(f"make {makeopts} modules_install | tee {log_dir}/modinstall-{log_timestamp}.log")
 if not modinstall_resp == 0:
-    print(f"!! Modules install failed with error code: {modinstall_resp}!")
-    print(f">> Check out modules install log at: {log_dir}/modinstall-{log_timestamp}.log")
+    print(Logger.error(f"!! Modules install failed with error code: {modinstall_resp}!"))
+    print(Logger.warning(f">> Check out modules install log at: {log_dir}/modinstall-{log_timestamp}.log"))
     exit(1)
 
 kinstall_resp = os.system(f"make {makeopts} install | tee {log_dir}/kinstall={log_timestamp}.log")
 if not kinstall_resp == 0:
-    print(f"!! Kernel install failed with error code: {kinstall_resp}!")
-    print(f">> Check out kernel install log at: {log_dir}/kinstall-{log_timestamp}.log")
+    print(Logger.error(f"!! Kernel install failed with error code: {kinstall_resp}!"))
+    print(Logger.warning(f">> Check out kernel install log at: {log_dir}/kinstall-{log_timestamp}.log"))
     exit(1)
 
 kver = os.popen("make -s kernelrelease").read();
-print(f">> Built and installed kernel: {kver}")
+print(Logger.success(f">> Built and installed kernel: {kver}"))
 
-print(">> Finally, constructing initramfs with the help of Dracut!")
+print(Logger.log(">> Finally, constructing initramfs with the help of Dracut!"))
 sleep(3)
 
-input("\nWarning! If you already have an initramfs with the same kernel version, it will be overwritten. Press Enter to continue or Ctrl+C to abort.")
+input(Logger.warning("\nWarning! If you already have an initramfs with the same kernel version, it will be overwritten. Press Enter to continue or Ctrl+C to abort."))
 
 dracut_resp = os.system(f"dracut --force --logfile {log_dir}/dracut-{log_timestamp}.log --kver {kver}")
 if not dracut_resp == 0:
-    print(f"!! Dracut exited with error code: {dracut_resp}!")
-    print(f">> Check out Dracut log at: {log_dir}/dracut-{log_timestamp}.log")
+    print(Logger.error(f"!! Dracut exited with error code: {dracut_resp}!"))
+    print(Logger.warning(f">> Check out Dracut log at: {log_dir}/dracut-{log_timestamp}.log"))
     exit(1)
 
-print("All done! You can now find the kernel and initramfs in the ESP directory.")
+print(Logger.success(">> All done! You can now find the kernel and initramfs in the ESP directory."))
 
 exit(0)
